8a90dec7e45a5a7df5aef9368026b48fadbabc8d
flyguywannadie <60896082+flyguywannadie@users.noreply.github.com>
2024-01-09 18:26:41 -0700

    roller

---
+++ Assets/Scripts/OrbitCamera.cs
@@ -0,0 +1,30 @@
+using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class OrbitCamera : MonoBehaviour
+{
+    [SerializeField] private Transform target;
+    //[SerializeField] private Vector3 camOffset;
+
+    [SerializeField] float mouseSensitivity = 0.2f;
+    [SerializeField, Range(20, 90)] float pitch = 40;
+    [SerializeField, Range(2, 10)] float distance = 5;
+
+    float yaw = 0;
+
+    // Update is called once per frame
+    void Update()
+    {
+        yaw += Input.GetAxis("Mouse X") * mouseSensitivity;
+
+        Quaternion qyaw = Quaternion.AngleAxis(yaw, Vector3.up);
+        Quaternion qpitch = Quaternion.AngleAxis(pitch, Vector3.right);
+
+        Quaternion rotation = qyaw * qpitch;
+
+        //transform.position = target.transform.position + camOffset;
+        transform.position = target.transform.position + (rotation * (Vector3.back * distance));
+        transform.rotation = rotation;
+    }
+}
